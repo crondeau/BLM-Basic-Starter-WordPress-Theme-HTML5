5e67251fd2574d842a7b194624987e51d23940b1
Christine Rondeau <christine@bluelimemedia.com>
2011-11-17 09:49:31 -0800

    Updated the header to include the html5.js file
    I forgot to include the js file initially. That file is needed forIE 8
    and lower.  I also updated the style.css with very minor changes.

--- header.php
+++ header.php
@@ -23,11 +23,14 @@
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/html5.js" type="text/javascript"></script>
+<![endif]-->
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <div id="wrap">
-	<header>
+	<header id="branding">
 		<div id="logo"><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></div>
 		<div id="tagline"><?php bloginfo('description'); ?></div>
 	</header>
